a30e3b42eb969d370b1e4596b5cb04c171e01d85
Jaidev Soin <jaidev.soin@gmail.com>
2020-05-23 14:28:10 +1000

    Adding username map

---
+++ docs/username_to_real_name_map.js
@@ -0,0 +1,15 @@
+(function() {
+  var nameMap = {
+    'Jai_': 'Jai',
+    'PENNOBLUFF69': 'Penno',
+    'Nick - Munter': 'Nick',
+    'Dazzla': 'Daz',
+    'EatMyShorts91': 'Kramer',
+    'Shaper000': 'Paula',
+    'Tom_From_Myspace': 'Harry',
+    'kiwis R us': 'Rhyse',
+    'Da_Saltedest_Sitta': 'Lockie',
+    'Tonsils McGee': 'Brad',
+    'Weekendpunter': 'Steu'
+  }
+})()
