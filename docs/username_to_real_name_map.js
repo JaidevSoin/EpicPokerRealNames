fc067ba05243b19e24cf282dad6e1acbddf44e6b
Jaidev Soin <jaidev.soin@gmail.com>
2020-05-24 10:38:36 +1000

    Testing list updating on the fly

--- docs/username_to_real_name_map.js
+++ docs/username_to_real_name_map.js
@@ -1,17 +1,13 @@
-(function() {
-  var usernameToRealNamesMap = {
-    'Jai_': 'Jai',
-    'PENNOBLUFF69': 'Penno',
-    'Nick - Munter': 'Nick',
-    'Dazzla': 'Daz',
-    'EatMyShorts91': 'Kramer',
-    'Shaper000': 'Paula',
-    'Tom_From_Myspace': 'Harry',
-    'kiwis R us': 'Rhyse',
-    'Da_Saltedest_Sitta': 'Lockie',
-    'Tonsils McGee': 'Brad',
-    'Weekendpunter': 'Steu'
-  }
-
-  window.startAddRealNamesLoop(usernameToRealNamesMap);
-})();
+window.usernameToRealNameMap = {
+  'Jai_': 'Jai',
+  'PENNOBLUFF69': 'Penno',
+  'Nick - Munter': 'Nick',
+  'Dazzla': 'Daz',
+  'EatMyShorts91': 'Kramer',
+  'Shaper000': 'Paula',
+  'Tom_From_Myspace': 'Harry',
+  'kiwis R us': 'Rhyse',
+  'Da_Saltedest_Sitta': 'Lockie',
+  'Tonsils McGee': 'Brad',
+  'Weekendpunter': 'Steu'
+}
