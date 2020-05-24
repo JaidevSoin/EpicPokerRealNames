6e295154f94896520acdf6e68546e1c035d2928c
Jaidev Soin <jaidev.soin@gmail.com>
2020-05-24 10:50:58 +1000

    Now fetches new list of usernames every 5 minutes

--- add_real_names.js
+++ add_real_names.js
@@ -1,40 +1,62 @@
 (function() {
+  window.usernameToRealNameMap = {}
 
-  // Get all the username elements in the page, check if the username has a matching real name, and if so, and it hasn't already been inserted, insert it
-  function addRealNames(usernameToRealNameMap) {
+  // Get all the username elements in the page, check if the username has a matching real name, and if so, and if hasn't already been inserted, insert it
+  function addRealNames() {
     var realNameClass = 'realName';
     var userNameElements = document.querySelectorAll('.player md-card-content .name');
 
     for (userNameElement of userNameElements) {
-      var realNameAlreadyAdded = userNameElement.parentNode.querySelectorAll('.' + realNameClass).length > 0
+      var userName = userNameElement.innerHTML;
 
-      if (!realNameAlreadyAdded) {
-        var userName = userNameElement.innerHTML;
+      if (userName in window.usernameToRealNameMap) {
+        // If we have a matching real name for a user name
+        var realNameElement;
+        var realName = window.usernameToRealNameMap[userName];
 
-        if (userName in usernameToRealNameMap) {
-          var realName = usernameToRealNameMap[userName];
+        // First check if we already have an element that contains a real name
+        realNameElement = userNameElement.parentNode.querySelector('.' + realNameClass);
 
-          var realNameElement = document.createElement('span');
+        if (realNameElement === null) {
+          // If this doesn't exist already, add the element along with the real name
+          realNameElement = document.createElement('span');
           realNameElement.innerHTML = realName;
           realNameElement.className = realNameClass;
           realNameElement.style = 'margin: 6px 0 -2px 0; font-size: .75rem';
           userNameElement.parentNode.insertBefore(realNameElement, userNameElement);
+
+        } else if (realNameElement.innerHTML != realName) {
+          // Otheriwise, check if the element already contains the correct real name, and if not, replace it
+          realNameElement.innerHTML = realName;
         }
       }
     }  
   }
 
-  // Start a loop that calls the above function every 5 seconds, passing in the list of real names
-  window.startAddRealNamesLoop = function(usernameToRealNameMap) {
-    setInterval(function(){
-      addRealNames(usernameToRealNameMap);
-    }, 5000);
+  function fetchRealNamesList() {
+    // Request the list of real names
+    var usernameToRealNameMapScript = document.createElement('script');
+    var random = Math.round(Math.random() * 100000000);
+    usernameToRealNameMapScript.setAttribute('src','https://jaidevsoin.github.io/EpicPokerRealNames/username_to_real_name_map.js?random=' + random);
+    document.head.appendChild(usernameToRealNameMapScript);
   }
 
-  // Request the list of real names
-  var usernameToRealNameMapScript = document.createElement('script');
-  var random = Math.round(Math.random() * 100000000);
-  usernameToRealNameMapScript.setAttribute('src','https://jaidevsoin.github.io/EpicPokerRealNames/username_to_real_name_map.js?random=' + random);
-  document.head.appendChild(usernameToRealNameMapScript);
-  
+  // Start a loop that calls addRealNames() every 5 seconds
+  function startAddRealNamesLoop() {
+    setInterval(function(){
+      addRealNames();
+    }, 5 * 1000);
+  }
+
+  // Start a loop that calls fetchRealNamesList() every 5 minutes
+  function startFetchRealNamesListLoop() {
+    fetchRealNamesList();
+
+    setInterval(function(){
+      fetchRealNamesList();
+    }, 5 * 60 * 1000);
+  }
+
+  startAddRealNamesLoop();
+  startFetchRealNamesListLoop();
 })();
