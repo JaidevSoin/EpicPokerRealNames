73d8ffbca6ea388674e99bb727e1f277944431cd
Jaidev Soin <jaidev.soin@gmail.com>
2020-05-28 18:11:05 +1000

    Adding icons, updating with a longer list

--- docs/username_to_real_name_map.js
+++ docs/username_to_real_name_map.js
@@ -9,5 +9,10 @@ window.usernameToRealNameMap = {
   'kiwis R us': 'Rhyse',
   'Da_Saltedest_Sitta': 'Lockie',
   'Tonsils McGee': 'Brad',
-  'Weekendpunter': 'Steu'
+  'Weekendpunter': 'Steu',
+  'Kelly Goosecock': 'Bret',
+  'DOWZ4': 'Dowza',
+  'Horrorshow': 'Horatio',
+  'HjvC': 'Henry',
+  'Jaybles2': 'Jesse'
 }
