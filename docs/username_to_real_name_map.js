f109895ac8ad85c66199f8220f50eabe97fc320f
Jaidev Soin <jaidev.soin@gmail.com>
2022-01-15 00:02:16 +1100

    Adding Doogs new name

--- docs/username_to_real_name_map.js
+++ docs/username_to_real_name_map.js
@@ -41,6 +41,7 @@ window.usernameToRealNameMap = {
   'Damo2000': 'Damo',
   'Doogsy': 'Dooley',
   'Doogs': 'Dooley',
+  'Ruse403': 'Dooley',
   'Shillyshallying159': 'Dooley',
   'Doogs37': 'Dooley',
   'Melics297': 'Tenzin',
